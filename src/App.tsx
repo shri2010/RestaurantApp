6df9dab82fa432937d9a6680fb4e1970e0c95a30
Sher Singh <shrit2010@gmail.com>
2022-11-25 21:53:38 +0530

    Added About us

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from './Navbar/Navbar'
 import { Footer } from './Footer/Footer'
 import { Header } from './header/Header'
+import AboutUs from './aboutUs/AboutUs'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Header />
+      <AboutUs />
       <Footer />
     </div>
   )
